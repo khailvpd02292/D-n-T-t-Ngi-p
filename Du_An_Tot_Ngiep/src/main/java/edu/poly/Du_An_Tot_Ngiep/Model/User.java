cc68328de7446a00d6821c3244ba4a0f7cb89fe1
nguyenvansonk35a3 <nguyenvansonk35a3@gmail.com>
2019-09-15 01:59:04 +0700

    create model user

---
+++ Du_An_Tot_Ngiep/src/main/java/edu/poly/Du_An_Tot_Ngiep/Model/User.java
@@ -0,0 +1,94 @@
+package edu.poly.Du_An_Tot_Ngiep.Model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+	
+	private int userId;
+	private String username;
+	private String password;
+	private String email;
+	private String phone;
+	private Date birthday;
+	private boolean gender;
+	private String address;
+	private boolean role;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean isRole() {
+		return role;
+	}
+
+	public void setRole(boolean role) {
+		this.role = role;
+	}
+
+}
